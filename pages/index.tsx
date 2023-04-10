ac818cf339ca6c98e779fd3c7bd9b74ae1d74d67
Ilya Holuban <ilyholu@gmail.com>
2023-04-10 19:38:38 +0300

    logout button, protected route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-light text-4xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque saepe accusamus quae eos nostrum facilis velit, voluptate consequuntur! Ipsa magnam natus nam, esse nihil voluptas odio repudiandae voluptatibus reprehenderit voluptates.</h1>
-
+    <div className="flex w-full justify-center align-middle m-5">
+      <button className="p-2 bg-red-600 text-white " onClick={() => signOut()}>
+        Logout
+      </button>
     </div>
-
-  )
+  );
 }
